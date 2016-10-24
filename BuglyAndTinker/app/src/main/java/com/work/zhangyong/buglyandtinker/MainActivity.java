98d5a1317e3a37718d5e75e539272934f9a5f383
mingzhangyong <504880928@qq.com>
2016-10-24 20:55:12 +0800

    bugly and tinker

---
+++ BuglyAndTinker/app/src/main/java/com/work/zhangyong/buglyandtinker/MainActivity.java
@@ -0,0 +1,26 @@
+package com.work.zhangyong.buglyandtinker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.tencent.bugly.crashreport.CrashReport;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView textView = (TextView) findViewById(R.id.hello);
+        CrashReport.initCrashReport(this);
+
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                CrashReport.testJavaCrash();
+            }
+        });
+    }
+}
