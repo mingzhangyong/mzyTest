b37b2fe40fc32d1bb4cb149dde38608cf54dc298
mingzhangyong <504880928@qq.com>
2016-10-25 17:55:51 +0800

    tinker first

---
+++ TinkerAndBugly/app/build/generated/source/apt/debug/com/work/zhangyong/tinkerandbugly/app/SampleApplication.java
@@ -0,0 +1,16 @@
+package com.work.zhangyong.tinkerandbugly.app;
+
+import com.tencent.tinker.loader.app.TinkerApplication;
+
+/**
+ *
+ * Generated application for tinker life cycle
+ *
+ */
+public class SampleApplication extends TinkerApplication {
+
+    public SampleApplication() {
+        super(7, "com.work.zhangyong.tinkerandbugly.app.SampleApplicationLike", "com.tencent.tinker.loader.TinkerLoader", false);
+    }
+
+}
---
+++ TinkerAndBugly/app/build/generated/source/buildConfig/androidTest/debug/tinker/sample/android/test/BuildConfig.java
@@ -0,0 +1,17 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package tinker.sample.android.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "tinker.sample.android.test";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.0";
+  // Fields from default config.
+  public static final String CLIENTVERSION = "mingzhangyong";
+  public static final String MESSAGE = "I am the base apk";
+  public static final String PLATFORM = "all";
+}
