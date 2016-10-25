428c216b4f5820cb2f2aa688084ebc7de2f8071d
mingzhangyong <504880928@qq.com>
2016-10-25 14:02:14 +0800

    test

---
+++ BuglyAndTinker/app/src/main/java/com/work/zhangyong/buglyandtinker/BuglyAndTinkerApplicationLike.java
@@ -0,0 +1,68 @@
+package com.work.zhangyong.buglyandtinker;
+
+import android.annotation.TargetApi;
+import android.app.Application;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.AssetManager;
+import android.content.res.Resources;
+import android.os.Build;
+import android.support.multidex.MultiDex;
+
+import com.tencent.bugly.crashreport.CrashReport;
+import com.tencent.tinker.anno.DefaultLifeCycle;
+import com.tencent.tinker.lib.tinker.TinkerInstaller;
+import com.tencent.tinker.loader.app.DefaultApplicationLike;
+import com.tencent.tinker.loader.shareutil.ShareConstants;
+import com.work.zhangyong.buglyandtinker.Log.MyLogImp;
+import com.work.zhangyong.buglyandtinker.util.SampleApplicationContext;
+import com.work.zhangyong.buglyandtinker.util.TinkerManager;
+
+/**
+ * Created by zhangyong on 2016/10/25.
+ */
+
+@SuppressWarnings("unused")
+@DefaultLifeCycle(application = ".BugLyAndTinkerApplication",
+        flags = ShareConstants.TINKER_ENABLE_ALL,
+        loadVerifyFlag = false)
+public class BuglyAndTinkerApplicationLike extends DefaultApplicationLike {
+    public BuglyAndTinkerApplicationLike(Application application, int tinkerFlags, boolean tinkerLoadVerifyFlag, long applicationStartElapsedTime, long applicationStartMillisTime, Intent tinkerResultIntent, Resources[] resources, ClassLoader[] classLoader, AssetManager[] assetManager) {
+        super(application, tinkerFlags, tinkerLoadVerifyFlag, applicationStartElapsedTime, applicationStartMillisTime, tinkerResultIntent, resources, classLoader, assetManager);
+    }
+
+
+    /**
+     * install multiDex before install tinker
+     * so we don't need to put the tinker lib classes in the main dex
+     *
+     * @param base
+     */
+    @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
+    @Override
+    public void onBaseContextAttached(Context base) {
+        super.onBaseContextAttached(base);
+        //you must install multiDex whatever tinker is installed!
+        MultiDex.install(base);
+
+        SampleApplicationContext.application = getApplication();
+        SampleApplicationContext.context = getApplication();
+        TinkerManager.setTinkerApplicationLike(this);
+        TinkerManager.initFastCrashProtect();
+        //should set before tinker is installed
+        TinkerManager.setUpgradeRetryEnable(true);
+
+        //optional set logIml, or you can use default debug log
+        TinkerInstaller.setLogIml(new MyLogImp());
+
+        //installTinker after load multiDex
+        //or you can put com.tencent.tinker.** to main dex
+        TinkerManager.installTinker(this);
+        CrashReport.initCrashReport(getApplication().getApplicationContext(), "900056991", false);
+    }
+
+    @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
+    public void registerActivityLifecycleCallbacks(Application.ActivityLifecycleCallbacks callback) {
+        getApplication().registerActivityLifecycleCallbacks(callback);
+    }
+}
--- BuglyAndTinker/app/src/main/java/com/work/zhangyong/buglyandtinker/MainActivity.java
+++ BuglyAndTinker/app/src/main/java/com/work/zhangyong/buglyandtinker/MainActivity.java
@@ -2,6 +2,7 @@ package com.work.zhangyong.buglyandtinker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
@@ -16,10 +17,14 @@ public class MainActivity extends AppCompatActivity {
         TextView textView = (TextView) findViewById(R.id.hello);
         CrashReport.initCrashReport(this);
 
+
+
         textView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 CrashReport.testJavaCrash();
+
+                Log.i("mzy","hahahahaweeew23232");
             }
         });
     }
